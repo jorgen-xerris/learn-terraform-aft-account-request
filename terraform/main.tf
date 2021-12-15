fff37bc19abe5f3566abdaf125b79b4ebf9150ac
Jorge Nava <jorge.nava@xerris.com>
2021-12-15 05:55:48 +0000

    Create sandbox-aft - 1 - Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "sandbox1" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "jorge.nava+sandbox1@xerris.com"
+    AccountName               = "sandbox-aft1-xerris"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = "jorge.nava+sandbox1@xerris.com"
+    SSOUserFirstName          = "Sandbox1"
+    SSOUserLastName           = "AFT1"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT1"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
